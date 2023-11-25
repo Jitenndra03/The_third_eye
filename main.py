3b89cde094faa35f7d3fd62cf90cd71a8e6d4925
Jitendra singh <113396507+Jitenndra03@users.noreply.github.com>
2023-11-26 02:02:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,61 +1,29 @@
+from ultralytics import YOLO
 import cv2
-import numpy as np
-
-# Load pre-trained MobileNetSSD model for object detection
-net = cv2.dnn.readNetFromCaffe("C:\Users\Dell\Python\Project\MobileNetSSD_deploy.prototxt", "C:\Users\Dell\Python\Project\MobileNetSSD_deploy.caffemodel")
-
-def detect_mobile_phone(frame):
-    # Resize frame to have a maximum width of 600 pixels (for faster processing)
-    frame_resized = cv2.resize(frame, (600, 600))
-    height, width = frame_resized.shape[:2]
-
-    # Preprocess the image for object detection
-    blob = cv2.dnn.blobFromImage(frame_resized, 0.007843, (300, 300), 127.5)
-
-    # Set the blob as input to the network
-    net.setInput(blob)
-
-    # Run forward pass to get detections
-    detections = net.forward()
-
-    # Loop over the detections
-    for i in range(detections.shape[2]):
-        confidence = detections[0, 0, i, 2]
-
-        # If confidence is above a certain threshold (e.g., 20%)
-        if confidence > 0.2:
-            class_id = int(detections[0, 0, i, 1])
-
-            # MobileNetSSD class label for "cell phone" is 77
-            if class_id == 77:
-                # Calculate bounding box coordinates
-                box = detections[0, 0, i, 3:7] * np.array([width, height, width, height])
-                (startX, startY, endX, endY) = box.astype("int")
-
-                # Draw the bounding box and label on the frame
-                cv2.rectangle(frame_resized, (startX, startY), (endX, endY), (0, 255, 0), 2)
-                cv2.putText(frame_resized, "Mobile Phone", (startX, startY - 15),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-    return frame_resized
-
-# Capture video from webcam (you can also read from a video file)
+import cvzone
+import math
 cap = cv2.VideoCapture(0)
+cap.set(3,1280)
+cap.set(4,720)
+model = YOLO("../yolo_weights/yolov8l.pt")
+
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop     sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange","broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
 
 while True:
-    # Read a frame from the webcam
-    ret, frame = cap.read()
-
-    # Detect mobile phones in the frame
-    result_frame = detect_mobile_phone(frame)
-
-    # Display the resulting frame
-    cv2.imshow("Mobile Phone Detection", result_frame)
-
-    # Break the loop if 'q' key is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Release the video capture object and close all windows
-cap.release()
-cv2.destroyAllWindows()
+    success, img = cap.read()
+    results = model(img, stream = True)
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            #bounding box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            w, h = x2-x1, y2-y1
+            cvzone.cornerRect(img, (x1, y1, w, h))
+            #confidence
+            conf = math.ceil((box.conf[0]*100))/100
+            #Class name
+            cls = int(box.cls[0])
+            cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(35, y1)))
+    cv2.imshow("Image",img)
+    cv2.waitKey(1)
